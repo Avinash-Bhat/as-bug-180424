7843b49a815f6ae63b391587e97b7a7c696a3742
Avinash R <nashpapa@gmail.com>
2015-07-27 15:03:52 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Introduction
+===
+
+This repository contains the details on how to regenerate the android-studio bug [#180424](http://b.android.com/180424).
+
+The [commits](https://github.com/Avinash-Bhat/as-bug-180424/commits/master) of this repository will be served as the instructions on recreating the bug.
---
+++ app/src/main/java/bugs/as/linkedresourcesbug/MainActivity.java
@@ -0,0 +1,15 @@
+package bugs.as.linkedresourcesbug;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView messageView = (TextView) findViewById(R.id.message);
+    }
+}
